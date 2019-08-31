51abf1ae65a95a66ebb5fd34fb0ef20f1987e545
Shady-96 <vv96rca@gmail.com>
2019-08-31 18:21:33 +0530

    third

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HistoryComponent } from './history/history.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'dashboard',component:DashboardComponent},
+  {path:'history',component:HistoryComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'**',component:PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const  PageRoutingModule =[DashboardComponent]
